91ff34266c38dc4b9114938aa840e48f4baefc4e
Jan Vermeerbergen <janvermeerbergen@gmail.com>
2023-12-19 22:14:05 +0100

    Product / Category entities

---
+++ ThePetShopBoysV2/src/main/java/be/ehb/ThePetShopBoysV2/model/Category.java
@@ -0,0 +1,67 @@
+package be.ehb.ThePetShopBoysV2.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Category name is required.")
+    @Size(min = 1, max = 255, message = "Category name must be between 1 and 255 characters long.")
+    private String name;
+
+    @Column(length = 1000)
+    @Size(max = 1000, message = "Description must not exceed 1000 characters.")
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Product> products = new HashSet<>();
+
+    public Category() {
+        // Default constructor
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+}
