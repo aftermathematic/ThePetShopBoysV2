3ea056bd3a355f310713924c1223fdca2e89895e
Jan Vermeerbergen <janvermeerbergen@gmail.com>
2023-12-19 21:39:22 +0100

    Initial commit

---
+++ ThePetShopBoysV2/src/main/java/be/ehb/ThePetShopBoysV2/ThePetShopBoysV2Application.java
@@ -0,0 +1,13 @@
+package be.ehb.ThePetShopBoysV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePetShopBoysV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThePetShopBoysV2Application.class, args);
+	}
+
+}
